3b0858638cf6fbe7680d6d27ba7eca4ca13697c7
Tlotlo Golelelwang <tlotlogolelelwang@gmail.com>
2024-10-14 11:53:43 +0200

    Responsive#1
    Making the app responsive

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ function SearchBar() {
   return (
       <div className = "searchbar">
           <form>
-              <input>
+              <input type= "text">
               
               </input>
           </form>
