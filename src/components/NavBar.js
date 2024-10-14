3a6e4d6580f40c9703e6dbb0fcd4b40d3cccde5b
Tlotlo Golelelwang <tlotlogolelelwang@gmail.com>
2024-10-14 15:45:10 +0200

    SearchBar
    Added a searchbar (functionality will be added tomorrow) 15 Oct 2024

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faNewspaper, faCloud, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 function Sidebar() {
   return (
     <div className="side-bar">
+
+          <div className = "search-bar">
+              <input className="input-location" type="text" placeholder ="Enter City" >
+          
+              </input>
+              <button>
+                <FontAwesomeIcon icon ={faMagnifyingGlass}/>
+              </button>
+          </div>
+    
       <ul>
+      
         <li>
-          <Link to="/">Weather</Link>
+      
+          <Link to="/">Weather <FontAwesomeIcon icon={faCloud}/>
+          </Link>
         </li>
         <li>
-          <Link to="/news">News</Link>
+          <Link to="/news">News <FontAwesomeIcon icon={faNewspaper} />
+          </Link>
         </li>
       </ul>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 function SearchBar() {
   return (
-      <div className = "searchbar">
-          <form>
-              <input type= "text">
-              
-              </input>
-          </form>
-      </div>
+    <div className = "search-bar">
+        <input className="input-location" type="text" placeholder ="Enter City" >
+
+        </input>
+        <button>
+          <FontAwesomeIcon icon ={faMagnifyingGlass}/>
+        </button>
+    </div>
     );
   }
   
